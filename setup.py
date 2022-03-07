c910e4ff34fdb6c609c05c4bbf93fd5e104fe91e
fuzihaofzh <fllubo@qq.com>
2022-03-07 19:26:34 +0800

    more ls

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cam-tool',
-    version='0.0.2',
+    version='0.0.5',
     description='Cloud Assignment Manager Tool',
     url='https://github.com/fuzihaofzh/cam-tool',
     author='',
